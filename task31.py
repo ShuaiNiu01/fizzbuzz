bdff9f32517e98aac7f16e8404f4047ef0e5b66a
ShuaiNiu01 <Shuai.Niu@dal.ca>
2020-02-20 16:45:34 -0400

    second commit

--- task31.py
+++ task31.py
@@ -5,6 +5,14 @@ def unique_words():
     book1 = open("Book1.txt","r")
     book2 = open("Book2.txt","r")
     book3 = open("Book3.txt","r")
+    file = open("20k.txt","r")
+    count1 = 0
+    lst = []
     for i in book1:
-        line = book1.readline()
-        
+        for j in file:
+
+            bookline = book1.readline()
+            textline = file.readline()
+            if textline in bookline:
+                lst.append(textline)
+                
