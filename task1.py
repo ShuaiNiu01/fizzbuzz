f05182aacb3f7cad279738faa650fbda746a5529
ShuaiNiu01 <Shuai.Niu@dal.ca>
2020-02-20 14:09:11 -0400

    second commit

--- task1.py
+++ task1.py
@@ -1,9 +1,20 @@
 import csv
 
 def verift_data():
-
+    GOOD = []
+    POOR = []
+    MARGINAL = []
     csvfile = open("Trails.csv","r")
     reader = csv.reader(csvfile1)
     for item in reader:
         if reader1.line_num == 1:
             continue
+        if item[9] == "GOOD":
+            GOOD.append(item[1])
+        if item[9] == "POOR":
+            POOR.append(item[1])
+        if item[9] == "MARGINAL":
+            MARGINAL.append(item[1])
+    print(GOOD)
+    print(POOR)
+    print(MARGINAL)
