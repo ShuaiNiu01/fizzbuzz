56bc01327f0c47123c367f8cc46155dfb390c3be
ShuaiNiu01 <Shuai.Niu@dal.ca>
2020-02-20 14:03:56 -0400

    first commit

---
+++ task1.py
@@ -0,0 +1,9 @@
+import csv
+
+def verift_data():
+
+    csvfile = open("Trails.csv","r")
+    reader = csv.reader(csvfile1)
+    for item in reader:
+        if reader1.line_num == 1:
+            continue
