98b4328aef1d63fc9e51af1a676e4bea2d773a61
ShuaiNiu01 <Shuai.Niu@dal.ca>
2020-02-20 16:40:20 -0400

    first commit

---
+++ task31.py
@@ -0,0 +1,10 @@
+
+
+
+def unique_words():
+    book1 = open("Book1.txt","r")
+    book2 = open("Book2.txt","r")
+    book3 = open("Book3.txt","r")
+    for i in book1:
+        line = book1.readline()
+        
