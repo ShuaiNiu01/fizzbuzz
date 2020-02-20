044ba90169b8761f71ee291886174ff959f7907c
ShuaiNiu01 <Shuai.Niu@dal.ca>
2020-02-20 15:31:50 -0400

    second commit

--- task2.py
+++ task2.py
@@ -1,7 +1,13 @@
 sourcefile = open("Book1.txt","r")
-destnatefile = open("copybook.txt","w")
+destnatefile = open("copybook.txt","a")
 # a = sourcefile.read()
 # print(a)
+count = 0
+page1 = 25
+pages = input("Please input how many pages you want to read: ")
 
-for i in sourcefile.read():
-    destnatefile.write(i)
+
+
+for i in range(int(pages) * page1):
+    line = sourcefile.readline()
+    destnatefile.write(line)
