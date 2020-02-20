30beaf20eaee81c340a5d9e3b7c0d9f35f57f080
ShuaiNiu01 <Shuai.Niu@dal.ca>
2020-02-20 15:47:35 -0400

    4th commit

--- task2.py
+++ task2.py
@@ -1,7 +1,6 @@
 sourcefile = open("Book1.txt","r")
 destnatefile = open("copybook.txt","a")
-# a = sourcefile.read()
-# print(a)
+
 count = 0
 page1 = 25
 pages = input("Please input how many pages you want to read: ")
@@ -12,4 +11,20 @@ choose = input("Please choose the number that you want to make change: ")
 
 for i in range(int(pages) * page1):
     line = sourcefile.readline()
+    line =
     destnatefile.write(line)
+
+def change_str1(line):
+    for i in line:
+        if 'o' in line:
+            index1 = line.find('o')
+            line = line[:index1] + '0' + line[index1+1:]
+        elif 'O' in line:
+            index2 = line.find('O')
+            line = line[:index2] + '0' + line[index2+1:]
+        elif 'a' in line:
+            index3 = line.find('a')
+            line = line[:index3] + '4' + line[index3+1:]
+        elif 'A' in line:
+            index4 = line.find('a')
+            line = line[:index4] + '4' + line[index4+1:]
